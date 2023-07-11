d0a14d6d3ff30d0b44b81c1ea1f01aebd1c8b03d
wodls1616 <wodls1616@gmail.com>
2023-07-11 10:48:44 +0900

    new MemoryMemberRepository가 중복을 해결 AppConfig를 리팩토링해서 역할과 구현이 한눈에 보이도록 수정을 하여 전체 구성을 빠르게 파악이 가능하도록 변경

--- spring_basic/src/main/java/com/basic/spring_basic/AppConfig.java
+++ spring_basic/src/main/java/com/basic/spring_basic/AppConfig.java
@@ -1,8 +1,10 @@
 package com.basic.spring_basic;
 
+import com.basic.spring_basic.member.MemberRepository;
 import com.basic.spring_basic.member.MemberService;
 import com.basic.spring_basic.member.MemberServiceImpl;
 import com.basic.spring_basic.member.MemoryMemberRepository;
+import com.basic.spring_basic.member.discount.DiscountPolicy;
 import com.basic.spring_basic.member.discount.FixDiscountPolicy;
 import com.basic.spring_basic.member.order.OrderService;
 import com.basic.spring_basic.member.order.OrderServiceImpl;
@@ -10,11 +12,19 @@ import com.basic.spring_basic.member.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
