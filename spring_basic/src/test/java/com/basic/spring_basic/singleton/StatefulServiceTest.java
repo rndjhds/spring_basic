b7aed91e2c3ad9722c5299b0b1c944020ca1635c
wodls1616 <wodls1616@gmail.com>
2023-07-13 01:46:18 +0900

    싱글톤을 무상태로 유지하지 않을경우 발생하는 문제점을 테스트 실행 무상태를 유지하지 않을 경우 인스턴스 필드의 값이 변경되는 문제점이 발생한다.

---
+++ spring_basic/src/test/java/com/basic/spring_basic/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package com.basic.spring_basic.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        // ThreadA : B사용자 10000원 주문
+        statefulService2.order("userB", 20000);
+
+        // ThreadA : 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
