863e8df91c3c4430a5fbf47843cfdc366d755510
wodls1616 <wodls1616@gmail.com>
2023-07-13 15:29:04 +0900

    빈 이름이 중복될 경우 자동 vs 자동, 수동 vs 자동으로 나누어서 결과를 확인해보고 SpringBootApplication을 실행 시켜 Spring Boot에서 어떻게 막았는지 확인하였다.

--- spring_basic/src/main/java/com/basic/spring_basic/AutoAppConfig.java
+++ spring_basic/src/main/java/com/basic/spring_basic/AutoAppConfig.java
@@ -1,14 +1,22 @@
 package com.basic.spring_basic;
 
+import com.basic.spring_basic.member.MemberRepository;
+import com.basic.spring_basic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "com.basic.spring_basic.member",
+        basePackages = "com.basic.spring_basic",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
