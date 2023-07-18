7ff1f298db992ae3e0d9cb88293ab2741d4398dd
wodls1616 <wodls1616@gmail.com>
2023-07-18 15:55:48 +0900

    웹 스코프의 request scope의 예제 코드를 만들어서 테스트 현재 이 코드는 HTTP 통신이 없기 때문에 request 객체를 생성하지 못한다. 그래서 실행시 오류 발생

---
+++ spring_basic/src/main/java/com/basic/spring_basic/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package com.basic.spring_basic.web;
+
+import com.basic.spring_basic.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
