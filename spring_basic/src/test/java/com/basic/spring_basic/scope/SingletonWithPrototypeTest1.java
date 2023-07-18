5b16121d417e9a324ab7d17568c6efbdd5d0d516
wodls1616 <wodls1616@gmail.com>
2023-07-18 10:39:45 +0900

    싱글톤 빈과 프로토타입 빈을 함께 사용할 시 이미 주입된 프로토타입 빈을 사용하고 새로운 프로토타입 빈을 생성하지 못한다는 단점이 있다. 단점을 확인하기 위한 테스트 코드 작성

--- spring_basic/src/test/java/com/basic/spring_basic/scope/SingletonWithPrototypeTest1.java
+++ spring_basic/src/test/java/com/basic/spring_basic/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,5 @@
 package com.basic.spring_basic.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -8,7 +7,7 @@ import org.springframework.context.annotation.Scope;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
     @Test
@@ -21,6 +20,39 @@ public class SingletonWithPrototypeTest1 {
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean; // 생성시점에 주입
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+
+
+
     }
 
     @Scope("prototype")
