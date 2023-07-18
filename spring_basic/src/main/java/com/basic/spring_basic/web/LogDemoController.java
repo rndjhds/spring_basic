91553a973903b5019189acec16226b7a4394621a
wodls1616 <wodls1616@gmail.com>
2023-07-18 16:26:11 +0900

    HTTP 통신이 없기 때문에 request 객체를 생성하지 못한다. 그래서 실행시 발생하는 오류를 Provider를 사용해서 빈 생성을 늦게 만들어줬다. 빈 생성을 늦게 해서 컨트롤러가 실행 될떄 request 객체가 생성이 되도록 변경

--- spring_basic/src/main/java/com/basic/spring_basic/web/LogDemoController.java
+++ spring_basic/src/main/java/com/basic/spring_basic/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.basic.spring_basic.web;
 
 import com.basic.spring_basic.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI().toString();
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
